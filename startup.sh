34bc9b15d1313ab134c6bdfb52b36c9cea969551
Eric Chio <im.ckieric@gmail.com>
2013-11-08 23:55:49 +0800

    Circular dependencies. Slight change in scripts.

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-# usage: git clone https://www.github.com/log0/aws_startup_scripts/ && cd aws_startup_scripts && ./startup.sh
+# usage: curl -L https://raw.github.com/log0/aws_startup_scripts/master/startup.sh | bash
 
 # get root
 sudo -s
@@ -7,7 +7,10 @@ sudo -s
 apt-get update
 apt-get install -y gcc g++ python-dev git htop gfortran libopenblas-dev liblapack-dev make
 
+git clone https://www.github.com/log0/aws_startup_scripts/
+
 # install custom-compiled libraries
+cd ~
 cd aws_startup_scripts
 
 tar zxvf hdf5-1.8.11.tar.gz
