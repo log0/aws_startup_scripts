a58cd16f495b6e59bd2a1b1abd269d1ec686f87a
Eric Chio <im.ckieric@gmail.com>
2013-11-08 17:59:26 +0800

    Added startup script.

---
+++ startup.sh
@@ -0,0 +1,22 @@
+# get root
+sudo -s
+
+# install related packages
+apt-get update
+apt-get install -y gcc g++ python-dev git htop gfortran libopenblas-dev liblapack-dev
+
+# mount our required disk
+cd ~
+mkdir ~/env
+mount /dev/xvdf1 ~/env
+
+# prepare keys
+cp ~/env/keys/* ~/.ssh
+
+# activate environment
+cd ~/env/source
+source bin/activate
+
+pip install numpy
+pip install scikit-learn
+pip install scipy
